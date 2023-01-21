bc89f931977c53cb014f2b6c73a60d28e5a139b2
Julian-dr-dev <julianmartinez21466@gmail.com>
2023-01-20 17:36:05 -0800

    light contribution

--- src/StudentManagement/RegistrationForm.java
+++ src/StudentManagement/RegistrationForm.java
@@ -4,17 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.nio.charset.StandardCharsets;
 
 public class RegistrationForm extends JDialog {
     private JTextField txtName;
     private JTextField numberTxt;
     private JTextField txtAddress;
     private JTextField txtUsername;
-    private JTextField txtPW;
-    private JTextField txtConfirmPw;
     private JButton registerBtn;
     private JButton cancelBtn;
     private JPanel registerPanel;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
 
     public RegistrationForm(JFrame parent) {
         super(parent);
@@ -23,23 +24,55 @@ public class RegistrationForm extends JDialog {
         setMaximumSize(new Dimension(450, 474));
         setModal(true);
         setLocationRelativeTo(parent);
-        setVisible(true);
         registerBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 registerUser();
             }
         });
+
         cancelBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                dispose();
             }
         });
+
+        setVisible(true);
+
     }
 
     private void registerUser() {
+        String password = String.valueOf(passwordField1.getPassword());
+        String confirmPw = String.valueOf(passwordField2.getPassword());
+        String name = txtName.getText();
+        String phoneNum = numberTxt.getText();
+        String address = txtAddress.getText();
+        String username = txtUsername.getText();
+
+        if (name.isEmpty()|| address.isEmpty() || phoneNum.isEmpty() || username.isEmpty() || password.isEmpty())
+        {
+            JOptionPane.showMessageDialog(this, "Please enter all fields", "Please try again", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (!password.equals(confirmPw))
+        {
+            JOptionPane.showMessageDialog(this, "Password does not match", "Please try again", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        addUserToDb(name, phoneNum, address, username, password);
+
     }
+    private User addUserToDb(String name, String phoneNum, String address, String username, String password)
+    {
+        User user = null;
+
+
+        return user;
+    }
+
 
     public static void main(String[] args) {
         RegistrationForm myForm = new RegistrationForm(null);
---
+++ src/StudentManagement/User.java
@@ -0,0 +1,10 @@
+package StudentManagement;
+
+public class User {
+
+    public String name;
+    public String phoneNum;
+    public String address;
+    public String username;
+    public String password;
+}
